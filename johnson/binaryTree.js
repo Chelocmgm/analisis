8d9c17aaf7ae4011349ebe43a70c11f3dedb2e6a
TeddyAP98 <100220773+TeddyAP98@users.noreply.github.com>
2022-05-18 12:18:49 -0400

    Update binaryTree.js

--- johnson/binaryTree.js
+++ johnson/binaryTree.js
@@ -1,9 +1,18 @@
+var vLabelNode=[];
+var exportButtonTree;
+
 function nodo(nom) {
   this.nom = nom;
   this.izq;
   this.der;
 }
 var res = "";
+
+function init() {
+  
+  exportButtonTree = document.getElementById("btnCargarArbol");
+
+}
 
 var prueba = {
   nom: 10,
@@ -35,6 +44,14 @@ var prueba = {
     },
   },
 };
+
+function addNodes(){
+  var nods=[];
+  nodes.forEach((node) => {
+    nods.push(node);
+  });
+  return nods;
+}
 
 function preOrder(cab) {
   res = "";
@@ -83,5 +100,72 @@ function inOrder(cab) {
     if (nodo === undefined || nodo === null) {
       return;
     }
+  }
+  
+  //guardar
+  function exportNetworkTree() {
+    Swal.fire({
+      title: "Ingrese el nombre del Archivo",
+      input: "text",
+      showCancelButton: true,
+      confirmButtonText: "Aceptar",
+      cancelButtonText: "Cancelar",
+      inputValidator: nombre => {
+          if (!nombre) {
+              return "Por favor escribe un nombre";
+          } else {
+              return undefined;
+          }
+      }
+  }).then(resultado => {
+      if (resultado.value) {           
+          vLabelNode=[];
+          let tree = document.getElementById("some-number").value; 
+          var exportValue = JSON.stringify({tree}, undefined, 1);
+          var archivo=resultado.value;
+          downloadObjectAsJson(exportValue, archivo);
+          Swal.fire(
+            'Completado',
+            'El archivo fue guardado exitosamente',
+            'success'
+          )
+      }
+  });
+    
+  }
+
+  var inputValueTree;
+  document.getElementById('btnCargarArbol').addEventListener( 
+    'change',  
+    changeEvent => { 
+      console.log("entro");
+      changeEvent.stopPropagation(); 
+      changeEvent.preventDefault(); 
+      readJsonFileTree(changeEvent.target.files[0]); 
+    }, 
+    false 
+  ); 
+
+  function readJsonFileTree(jsonFile) { 
+    var reader = new FileReader(); 
+    reader.addEventListener('load', (loadEvent) => { 
+      try { 
+        inputValueTree = JSON.parse(loadEvent.target.result); 
+        importNetworkTree(inputValueTree);
+      } catch (error) { 
+        console.error(error); 
+      } 
+    }); 
+    reader.readAsText(jsonFile); 
+  } 
+  
+  function importNetworkTree(archive) {
+    //var inputData = JSON.parse(archive);
+    console.log(archive.tree);
+    tree = archive.tree;
+    console.log(tree);
+    var data = tree;
+    document.getElementById("some-number").value = data;
+  
   }
   
