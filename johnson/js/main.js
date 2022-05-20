e22a55443cef268b2cc68b1fc2bbfa39c2192809
MiguelBarrionuevo <88354350+MiguelBarrionuevo@users.noreply.github.com>
2022-05-20 09:46:32 -0400

    Update main.js

--- johnson/js/main.js
+++ johnson/js/main.js
@@ -961,7 +961,41 @@ function changeColor(route) {
         }
     }
 }
+//Parte de Sort
+let sorted = false;
 
+$(document).ready(() => {
+
+	// Sort button
+
+	$("#sort").click(() => {
+        if (sorted) {
+            $('section.errors p').text('Already sorted!');
+            return;
+        }
+        $("section.errors p").text("");
+        let $array = getArrayValue();
+        sort($array[0]);
+        sorted = true;
+    });
+    
+    // Unsort button
+
+    $("#unsort").click(() => {
+        if (!sorted) {
+            $("section.errors p").text("Already unsorted!");
+            return;
+        }
+        $("section.errors p").text("");
+        let sortedArray = $('div.array-container')[0];
+        let unsortedArray = [];
+        for (let i = sortedArray.childNodes.length - 1; i >= 0 ; i--)
+            unsortedArray.push($(sortedArray.childNodes[i]).text());
+        unsort(unsortedArray);
+        sorted = false;
+    });
+});
+//Fin Sort
 // Verify if the string is Empty.
 function valueIsEmpty(label) {
     return label && label !== "";
